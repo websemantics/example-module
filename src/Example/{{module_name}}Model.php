d2809c88d5353b922016251f281eeaea2fc9a82a
Adnan Sagar, PhD. <msagar@gmail.com>
2015-01-23 16:56:49 -0500

    Update and rename ExampleModel.php to {{module_name}}Model.php

---
+++ src/Example/{{module_name}}Model.php
@@ -0,0 +1,13 @@
+<?php namespace {{namespace}}{{module_name}};
+
+use Anomaly\Streams\Platform\Model\{{module_name}}\{{module_name}}EntryModel;
+
+class {{module_name}}Model extends {{module_name}}EntryModel
+{
+
+    public function isAwesome()
+    {
+        return true;
+    }
+}
+ 
