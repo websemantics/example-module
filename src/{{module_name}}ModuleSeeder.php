a97dc12ffc65502240a1d2f9a9522e1fb4e7595a
Adnan <adnan@websemantics.ca>
2015-04-07 10:53:51 -0400

    add seeder class

---
+++ src/{{module_name}}ModuleSeeder.php
@@ -0,0 +1,14 @@
+<?php namespace {{vendor_name}}\\{{module_name}}Module;
+
+use Anomaly\Streams\Platform\Database\Seeder\Seeder;
+
+class {{module_name}}ModuleSeeder extends Seeder
+{
+    /**
+     * Seed the localization module.
+     */
+    public function run()
+    {                
+        
+    }
+}
