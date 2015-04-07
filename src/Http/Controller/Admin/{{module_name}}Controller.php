6b1e1e7f52b607d0ae33c40be887054004cdb6ea
Adnan <adnan@websemantics.ca>
2015-04-07 10:43:49 -0400

    Update

---
+++ src/Http/Controller/Admin/{{module_name}}Controller.php
@@ -0,0 +1,18 @@
+<?php namespace {{vendor_name}}\\{{module_name}}Module\Http\Controller;
+
+use Anomaly\Streams\Platform\Http\Controller\AdminController;
+
+class {{module_name}}Controller extends AdminController
+{
+
+ /**
+   * Return master admin view.
+   *
+   * @return \Illuminate\View\View|\Symfony\Component\HttpFoundation\Response
+   */
+  public function index()
+  {
+       return view('{{toLowerCase vendor_name}}.module.{{toLowerCase module_name}}::admin.master');
+  }
+
+}
--- src/{{module_name}}ModuleServiceProvider.php
+++ src/{{module_name}}ModuleServiceProvider.php
@@ -1,19 +1,12 @@
 <?php namespace {{vendor_name}}\\{{module_name}}Module;
 
-use Illuminate\Support\ServiceProvider;
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
 
-class {{module_name}}ModuleServiceProvider extends ServiceProvider
+class {{module_name}}ModuleServiceProvider extends AddonServiceProvider
 {
 
-    public function register()
-    {
-        $this->app->make('router')->get(
-            'admin/{{toLowerCase module_name}}',
-            '{{vendor_name}}\\{{module_name}}Module\Http\Controller\\{{module_name}}Controller@index'
-        );
-        $this->app->make('router')->get(
-            'admin/{{toLowerCase module_name}}/create',
-            '{{vendor_name}}\\{{module_name}}Module\Http\Controller\\{{module_name}}Controller@create'
-        );
-    }
+    protected $routes = [
+    'admin/{{toLowerCase module_name}}' => '{{vendor_name}}\\{{module_name}}Module\Http\Controller\Admin\\{{module_name}}Controller@index'
+    ];
+
 }
